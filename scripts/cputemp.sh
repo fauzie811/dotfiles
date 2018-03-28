e8fcbf54308d3feac52afdafef98f1cf8b3eea0d
Fauzie Rofi <fauzie811@yahoo.com>
2018-03-28 14:32:03 +0700

    updates

--- scripts/cputemp.sh
+++ scripts/cputemp.sh
@@ -1,2 +1,4 @@
-echo "$HOME/scripts/icons/cpu.svg"
-echo $(sensors | grep -A 1 -m 1 'temp1' | cut -c16-17 | sed '/^$/d')°C
+TEMP=$(sensors | grep -A 1 -m 1 'temp1' | cut -c16-17 | sed '/^$/d')
+
+echo "$HOME/scripts/icons/thermometer-$((($TEMP-40)*5/50)).svg"
+echo $TEMP°C
