f26f102345efdfad6b504cc63f243f0a1d33c6ef
Fauzie <fauzie811@yahoo.com>
2021-12-22 19:48:25 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
-ln -s $PWD/.config/nvim ~/.config/nvim
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -n 'Installing neovim config... '
+if [ -e ~/.config/nvim ]; then
+  echo -e "${RED}skipped${NC}"
+else
+  ln -s $PWD/.config/nvim ~/.config/nvim
+  echo -e "${GREEN}ok${NC}"
+fi
+
+echo -n 'Installing starship config... '
+if [ -e ~/.config/starship.toml ]; then
+  echo -e "${RED}skipped${NC}"
+else
+  ln -s $PWD/.config/starship.toml ~/.config/starship.toml
+  echo -e "${GREEN}ok${NC}"
+fi
