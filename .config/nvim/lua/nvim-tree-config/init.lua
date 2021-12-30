16fa0b8d9382ba509dd18092a41ec8d8c6a836e4
Fauzie <fauzie811@yahoo.com>
2021-12-31 05:07:42 +0700

    nvim: update config

--- .config/nvim/lua/nvim-tree-config/init.lua
+++ .config/nvim/lua/nvim-tree-config/init.lua
@@ -1,28 +1,27 @@
-vim.g.nvim_tree_width = 25
-vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  files = 1,
+  folder_arrows = 1,
+}
 vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-      unstaged = "",
-      staged = "S",
-      unmerged = "",
-      renamed = "➜",
-      deleted = "",
-      untracked = "U",
-      ignored = "◌",
-    },
-    folder = {
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-    },
-  }
--- require'nvim-tree'.setup ()
+  default = "",
+  symlink = "",
+  folder = {
+    arrow_open = "",
+    arrow_closed = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
 require'nvim-tree'.setup {
+  hijack_cursor = true,
   update_cwd = true,
   update_focused_file = {
     enable = true,
@@ -36,10 +35,16 @@ require'nvim-tree'.setup {
       warning = "",
       error = "",
     },
-   git = {
+  },
+  git = {
     enable = true,
     ignore = false,
     timeout = 500,
-  }, },
+  },
+  view = {
+    width = 30,
+    allow_resize = true,
+    signcolumn = "no",
+  },
 }
 
--- .config/nvim/lua/telescope-config/init.lua
+++ .config/nvim/lua/telescope-config/init.lua
@@ -1,1 +1,8 @@
 -- require('telescope').load_extension('projects')
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
+}
